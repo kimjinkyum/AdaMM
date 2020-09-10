3995ad86efbc77c0ff8fb71a7285db03396802e9
kimjinkyum <kimo1113@daum.net>
2020-09-11 00:19:10 +0900

    Add motion tracking

--- EdgeNode/ex_queue.py
+++ EdgeNode/ex_queue.py
@@ -2,14 +2,52 @@ import multiprocessing as mp
 import time
 import socket
 import numpy as np
-#import socket.timeout as TimeoutException
+import argparse
+import cv2
+
 class Test(mp.Process):
-  def __init__(self, image):
-    super(Test, self).__init__()
-    self.image = image
-    
-  def run(self):
-      #Tensorflow 모델 불러오기
+    def __init__(self, image):
+        super(Test, self).__init__()
+        self.image = image
+        args, e,w,h=modle_init()
+        self.args=arg
+        self.e=e
+        self.w=w
+        self.h=h
+
+    def run(self):
+        from tf_pose.estimator import TfPoseEstimator
+        while True:
+            decimg=self.image.get()
+            if img=="Timeout":
+                print("StopProcess")
+                break
+            humans = self.e.inference(decimg, resize_to_default=(self.w > 0 and self.h > 0), upsample_size=self.args.resize_out_ratio)
+            y1=[0.0]
+            y=0
+            image = TfPoseEstimator.draw_humans(decimg, humans, imgcopy=False)
+            for human in humans:
+                for i in range(len(humans)):
+                    try :
+                        a=human.body_parts[0]
+                        x=a.x*image.shape[1]
+                        y=a.y*image.shape[0]
+                        y1.append(y)
+                        #print(y1[-2])
+                    except:
+                        pass
+                    if ((y-y1[len(y1)-2])>30):
+                        print("fall",i+1)
+                    #send_data={"name" :"fall"}
+                    #r=requests.post("http://127.0.0.1:5000/http",json=send_data)
+
+            cv2.putText(image,
+                        "FPS: %f" % (1.0 / (time.time() - fps_time)),
+                        (10, 10),  cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                        (0, 255, 0), 2)
+            cv2.imshow('tf-pose-estimation result', image)
+      
+      """
       while True:
           start_time=time.time()
           img=self.image.get()
@@ -21,8 +59,10 @@ class Test(mp.Process):
               #self.terminate()
               break
           #print("I'm the process with id: {}".format(img))
+      """
 
 def receving(img,queue):
+
     queue.put(img)
 
 def recvall(sock, count):
@@ -33,6 +73,51 @@ def recvall(sock, count):
         buf += newbuf
         count -= len(newbuf)
     return buf
+
+# For motion tracking function    
+def str2bool(v):
+    return v.lower() in ("yes", "true", "t", "1")
+
+def make_arg():
+    
+    parser = argparse.ArgumentParser(description='tf-pose-estimation realtime webcam')
+    parser.add_argument('--resize', type=str, default='0x0',
+                        help='if provided, resize images before they are processed. default=0x0, Recommends : 432x368 or 656x368 or 1312x736 ')
+    parser.add_argument('--resize-out-ratio', type=float, default=4.0,
+                        help='if provided, resize heatmaps before they are post-processed. default=1.0')
+
+    parser.add_argument('--model', type=str, default='mobilenet_thin', help='cmu / mobilenet_thin / mobilenet_v2_large / mobilenet_v2_small')
+    parser.add_argument('--show-process', type=bool, default=False,
+                        help='for debug purpose, if enabled, speed for inference is dropped.')
+    
+    parser.add_argument('--tensorrt', type=str, default="False",
+                        help='for tensorrt process.')
+    args = parser.parse_args()
+    return args
+
+def model_init():
+    from tf_pose.estimator import TfPoseEstimator
+    from tf_pose.networks import get_graph_path, model_wh
+    args=make_arg()
+    s=time.time()
+    print("start tensorflow")
+    w, h = model_wh(args.resize)
+    if w > 0 and h > 0:
+        e = TfPoseEstimator(get_graph_path(args.model), target_size=(w, h), trt_bool=str2bool(args.tensorrt))
+    else:
+        e = TfPoseEstimator(get_graph_path(args.model), target_size=(432, 368), trt_bool=str2bool(args.tensorrt))
+    print("Timing init",time.time()-s)
+    return args, e,w,h
+
+#open pose init 부분 
+logger = logging.getLogger('TfPoseEstimator-WebCam')
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
 
 if __name__ == '__main__':
     pqueue=mp.Queue()
@@ -73,7 +158,8 @@ if __name__ == '__main__':
         if length:
             stringData = recvall(conn, int(length))
             data = np.frombuffer(stringData, dtype='uint8')
-            receving(data[0],pqueue)            
+            decimg=cv2.imdecode(data,1)
+            receving(decimg,pqueue)            
         else:
             print("esle")
             break
