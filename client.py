2b1206ba471ab1d2a104c674df8a7733d126911d
kimjinkyum <kimo1113@daum.net>
2020-09-16 03:07:25 +0900

    Process (image data)

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import socket
 import time
 import numpy as np
+import cv2
 client_socket = socket.socket()
 
 ip_address="127.0.0.1" #Edge node Ip address
@@ -8,12 +9,19 @@ client_socket.connect((ip_address, 8000))  # ADD IP HERE
 
 connection = client_socket.makefile('wb')
 i=0
-a=""
+video_path="C:\\Users\\user01\\Desktop\\tf-pose-estimation\\t1.mp4"
+cam = cv2.VideoCapture(video_path)
+
 while True:
-    data = np.array(i)
+    ret, img = cam.read()
+    if ret is None:
+        break
+    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+    result, imgencode = cv2.imencode('.jpg', img, encode_param)
+    data = np.array(imgencode)
     stringData = data.tostring()
-    #client_socket.send(data.encode())
     time.sleep(0.5)
+
     if i==1:
         #client_socket.send(str(len(a)).ljust(16).encode())
         time.sleep(10)
@@ -22,6 +30,4 @@ while True:
         client_socket.send(str(len(stringData)).ljust(16).encode())
         client_socket.send(stringData)
 
-    if i>15:
-        break
     i+=1
